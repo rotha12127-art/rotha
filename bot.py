f9400b1f3f088f87e1780c2504098f3a37caa507
rotha12127-art <rotha.12127@gmail.com>
2026-08-19 01:02:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from telegram.ext import (
 
 # ==================== ការកំណត់ព័ត៌មាន (CONFIGURATION) ====================
 
-BOT_TOKEN = "8469005375:AAHXmdGpdM0DPZJYIaIhd4dBq9ZkdUbp-YM"
+BOT_TOKEN = "8469005375:AAHXmdGpdMOdPZJYIaIhd4dBq9ZkdUbp-YM"
 ADMIN_GROUP_ID = "-1004401338807"
 QR_CODE_FILE = "acleda_qr.png"
 
