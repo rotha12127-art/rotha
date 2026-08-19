c1ac25d8b29c201295ca1dccc4575472629ba683
rotha12127-art <rotha.12127@gmail.com>
2026-08-19 15:38:26 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,7 @@ SONGS_DATABASE = {
         "title": "Track 2",
         "price": "FREE",
         "is_free": True,       # ដោនឡូតបានភ្លាមៗ
-        "file_path": "Project_2.mp3",
+        "file_path": "5_6307483950365289011.mp3",
     },
     "song_3": {
         "title": "Track 3",
